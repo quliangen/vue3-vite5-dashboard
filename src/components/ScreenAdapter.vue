3368abaf7f30b0419a3d2fd1b41b63c65b5231c7
quliangen <quliangen@outlook.com>
2023-01-07 16:33:35 +0800

    feat: 修改大屏样式

--- src/components/ScreenAdapter.vue
+++ src/components/ScreenAdapter.vue
@@ -31,14 +31,8 @@ onMounted(() => {
   color: #fff;
   text-align: center;
   // [background-color] [background-image] [background-repeat] [background-attachment] [background-position] / [background-size] [background-origin] [background-clip]
-  background: url('../assets/img/dashboard/left-top-bg.svg') no-repeat left 31px top 31px / 25px 25px,
-    url('../assets/img/dashboard/right-top-bg.svg') no-repeat right 31px top 31px / 25px 25px,
-    url('../assets/img/dashboard/left-bottom-bg.svg') no-repeat left 31px bottom 31px / 25px 25px,
-    url('../assets/img/dashboard/right-bottom-bg.svg') no-repeat right 31px bottom 31px / 25px 25px,
-    url('@/assets/img/dashboard/top_bg.svg') no-repeat top center / 1600px 72px,
-    url('../assets/img/dashboard/bottom_bg.svg') no-repeat center bottom / 100%,
-    url('../assets/img/dashboard/left_bg.svg') no-repeat top 73px left / 106px 718px,
-    url('../assets/img/dashboard/right_bg.svg') no-repeat top 73px right / 106px 718px,
-    url('../assets/img/dashboard/main_bg.svg') no-repeat center / cover;
+  background: url('@/assets/img/dashboard/top_bg.svg') no-repeat top center / 1600px 72px,
+    url('../assets/img/dashboard/bottom_bg.svg') no-repeat center bottom / 100%;
+  background-color: #151c30;
 }
 </style>
